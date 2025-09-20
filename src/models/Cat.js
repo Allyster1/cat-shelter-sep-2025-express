fc0123a9f8117f4391097522a97f09d7f2eaaffb
Slavi Ovardov <ovardovslav@gmail.com>
2025-09-20 16:05:40 +0300

    create save method in movie model

--- src/models/Cat.js
+++ src/models/Cat.js
@@ -4,7 +4,19 @@ const dbSerialized = await fs.readFile("./src/db.json", { encoding: "utf-8" });
 let db = JSON.parse(dbSerialized);
 
 export default class Cat {
+   constructor(data) {
+      Object.assign(this, data);
+   }
+
    static find() {
       return db.cats.slice();
+   }
+
+   save() {
+      db.cats.push(this);
+
+      console.log(db.cats);
+
+      return this;
    }
 }
